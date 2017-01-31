9a8accc8d7841dadf0a4084a132fd4a7a24364bb
Matěj Sychra <igraczech@igraczech.com>
2017-01-31 15:15:50 +0100

    updated ESP code for easier first setup

---
+++ esp8266/config.lua
@@ -0,0 +1,3 @@
+wifi_ssid = '<ssid>'
+wifi_password = '<password>'
+mqtt_broker = '192.168.1.21'
--- esp8266/init.lua
+++ esp8266/init.lua
@@ -2,5 +2,5 @@
 local IDLE_AT_STARTUP_MS = 3000
 print("Will bootstrap in " .. IDLE_AT_STARTUP_MS .. " milliseconds...")
 tmr.alarm(1,IDLE_AT_STARTUP_MS,0,function()
-    dofile("rshield.lua") -- Security delay in case the app locks CPU
+    dofile("sonoff.lua") -- Security delay in case the app locks CPU
 end)
